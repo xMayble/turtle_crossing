7637cabb23bb665ece20c5884c25f6cc88eeacf3
Mehbub Rohit <mehburohit@gmail.com>
2024-02-26 23:57:49 -0500

    created turtle and added keypress

--- player.py
+++ player.py
@@ -6,8 +6,11 @@ FINISH_LINE_Y = 280
 
 class Player(Turtle):
     def __init__(self):
-        super().__init__()
+        super(Player, self).__init__()
         self.shape("turtle")
         self.penup()
         self.goto(STARTING_POSITION)
         self.setheading(90)
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
