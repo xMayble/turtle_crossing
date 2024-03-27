a476416745c6051e4aac7ee28c05c0783591e7fb
Mehbub Rohit <mehburohit@gmail.com>
2024-03-27 15:04:32 -0400

    turtle game

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from player import Player
 from cars import Cars
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +11,7 @@ screen.tracer(0)
 
 player = Player()
 cars = Cars()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -26,11 +28,13 @@ while game_is_on:
     for car in cars.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
     # Detect successful crossing
     if player.is_at_finish_line():
         player.go_to_start()
         cars.level_up()
+        scoreboard.increase_level()
 
 
 
